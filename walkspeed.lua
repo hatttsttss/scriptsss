33d3e3d49bac28fca601f15e60e2bc2257d46685
hatttsttss <quanwashington2013@icloud.com>
2025-07-28 16:00:26 -0500

    Update walkspeed.lua

--- walkspeed.lua
+++ walkspeed.lua
@@ -1,1 +1,20 @@
+-- JQHub WalkSpeed Script
+
+local player = game.Players.LocalPlayer
+local speed = 22 -- You can change this number if you want faster or slower
+
+local function applySpeed()
+    local char = player.Character or player.CharacterAdded:Wait()
+    local humanoid = char:WaitForChild("Humanoid", 5)
+    if humanoid then
+        humanoid.WalkSpeed = speed
+        print("[JQHub] WalkSpeed set to:", speed)
+    else
+        warn("[JQHub] Humanoid not found")
+    end
+end
+
+-- Apply immediately and on respawn
+applySpeed()
+player.CharacterAdded:Connect(applySpeed)
 
