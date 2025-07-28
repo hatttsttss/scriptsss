6ea0a40f25cc176a0fb7afc5845bbd83e2e666fc
hatttsttss <quanwashington2013@icloud.com>
2025-07-28 15:20:32 -0500

    Update walkspeed.lua

--- walkspeed.lua
+++ walkspeed.lua
@@ -1,1 +1,13 @@
+-- WalkSpeed Script (JQHub)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid", 5)
+if humanoid then
+    humanoid.WalkSpeed = 30
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "JQHub",
+        Text = "🏃 WalkSpeed set to 30",
+        Duration = 4
+    })
+end
 
