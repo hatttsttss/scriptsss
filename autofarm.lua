445b7891229fcfa36f6dfb644a394028324106e9
hatttsttss <quanwashington2013@icloud.com>
2025-07-28 16:05:28 -0500

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,1 +1,13 @@
+-- Marshmello AutoFarm
+local player = game.Players.LocalPlayer
+
+while true do
+    wait(1)
+    for _,v in pairs(workspace:GetDescendants()) do
+        if v:IsA("TouchTransmitter") and v.Parent then
+            firetouchinterest(player.Character.HumanoidRootPart, v.Parent, 0)
+            firetouchinterest(player.Character.HumanoidRootPart, v.Parent, 1)
+        end
+    end
+end
 
