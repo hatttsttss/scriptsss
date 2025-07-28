f72648c62c901100eb49d7c2e5f93beeeb5d1e53
hatttsttss <quanwashington2013@icloud.com>
2025-07-28 17:26:18 -0500

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,2 +1,30 @@
-local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/ESP.lua"))()
--- ESPLibrary and getgenv().ExunysDeveloperESP is equivalent.
+-- Simple ESP by JQHub
+local function createESP(plr)
+    if plr == game.Players.LocalPlayer then return end
+    local char = plr.Character or plr.CharacterAdded:Wait()
+    local box = Instance.new("BillboardGui", char:WaitForChild("Head"))
+    box.Name = "JQ_ESP"
+    box.Size = UDim2.new(4, 0, 0.5, 0)
+    box.AlwaysOnTop = true
+    local label = Instance.new("TextLabel", box)
+    label.Size = UDim2.new(1, 0, 1, 0)
+    label.BackgroundTransparency = 1
+    label.Text = plr.Name
+    label.TextColor3 = Color3.new(1, 0, 0)
+    label.TextStrokeTransparency = 0.5
+    label.TextScaled = true
+end
+
+for _, plr in pairs(game.Players:GetPlayers()) do
+    if not plr.Character or not plr.Character:FindFirstChild("Head") then
+        plr.CharacterAdded:Wait()
+    end
+    createESP(plr)
+end
+
+game.Players.PlayerAdded:Connect(function(plr)
+    plr.CharacterAdded:Connect(function()
+        task.wait(1)
+        createESP(plr)
+    end)
+end)
