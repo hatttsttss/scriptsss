12e575718bc3fc0ae459da031677322b078ebcc9
hatttsttss <quanwashington2013@icloud.com>
2025-07-29 00:00:37 -0500

    Update instantinteract.lua

--- instantinteract.lua
+++ instantinteract.lua
@@ -1,6 +1,6 @@
-for _,v in pairs(getgc(true)) do
-    if type(v) == "table" and rawget(v, "HoldTime") then
+-- Instant Interact Script
+for _, v in pairs(getgc(true)) do
+    if typeof(v) == "table" and rawget(v, "HoldTime") then
         v.HoldTime = 0
     end
 end
-
