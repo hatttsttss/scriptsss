3af875b57b30d3b2abc569e707cc8debf1db2cdd
hatttsttss <quanwashington2013@icloud.com>
2025-07-29 01:23:57 -0500

    Update instantinteract.lua

--- instantinteract.lua
+++ instantinteract.lua
@@ -1,6 +1,16 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-game:GetService("ProximityPromptService").PromptShown:Connect(function(prompt)
-  fireproximityprompt(prompt)
+local function fastInteract()
+    for _, v in pairs(getgc(true)) do
+        if typeof(v) == "table" and rawget(v, "HoldTime") and typeof(v.HoldTime) == "number" then
+            v.HoldTime = 0
+        end
+    end
+end
+
+task.spawn(function()
+    while true do
+        if _G.InstantInteractEnabled then
+            fastInteract()
+        end
+        task.wait(0.25)
+    end
 end)
