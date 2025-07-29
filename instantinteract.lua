bac58fe50c5045d2a8341874a9e10c264a2b7412
hatttsttss <quanwashington2013@icloud.com>
2025-07-29 00:07:13 -0500

    Update instantinteract.lua

--- instantinteract.lua
+++ instantinteract.lua
@@ -1,6 +1,21 @@
--- Instant Interact Script
-for _, v in pairs(getgc(true)) do
-    if typeof(v) == "table" and rawget(v, "HoldTime") then
-        v.HoldTime = 0
+CreateToggle("Instant Interact", function(state)
+    JQHubSettings.InstantInteract = state
+
+    if state then
+        local function patchHoldTime()
+            for _, v in pairs(getgc(true)) do
+                if typeof(v) == "table" and rawget(v, "HoldTime") and type(v.HoldTime) == "number" then
+                    v.HoldTime = 0
+                end
+            end
+        end
+
+        patchHoldTime()
+        task.spawn(function()
+            while JQHubSettings.InstantInteract do
+                patchHoldTime()
+                task.wait(1)
+            end
+        end)
     end
-end
+end)
